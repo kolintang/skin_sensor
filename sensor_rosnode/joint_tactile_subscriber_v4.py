3e0c38978877a8506884241c522373eb4b0179f6
skrillcii <skrillcii@github.com>
2017-12-11 18:38:59 +0900

    updates

--- sensor_rosnode/joint_tactile_subscriber_v4.py
+++ sensor_rosnode/joint_tactile_subscriber_v4.py
@@ -4,13 +4,6 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import String, Int32MultiArray
 
 
-# Setup TCP server
-TCP_IP = '192.168.11.11'
-TCP_PORT = 5080
-BUFFER_SIZE = 8192
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 # Initialise csv path and dataset name
 object_name = 'test'
 trial_number = '00'
@@ -18,23 +11,23 @@ csv_name = '../datasets/' + object_name + '_' + trial_number + '.csv'
 
 
 # Initialise lists
-finger_numer = 3
+finger_numer = 1
 patch_number = 5
 taxel_number = 16
 csv_header = []
-tactile_list_0 = [None]*48*patch_number
 tactile_list_1 = [None]*48*patch_number
-tactile_list_2 = [None]*48*patch_number
-tactile_list_3 = [None]*48*patch_number
+#tactile_list_2 = [None]*48*patch_number
+#tactile_list_3 = [None]*48*patch_number
+#tactile_list_4 = [None]*48*patch_number
 joint_list = [None]*16*patch_number
 JT_List = [None]*64*patch_number
 
 
 def csv_init():
     # Initialising taxel number header
-    for i in range (finger_number)
-        for j jn range (patch_number):
-            for k jn range (taxel_number):
+    for i in range (finger_number):
+        for j in range (patch_number):
+            for k in range (taxel_number):
                 csv_header.append(str(i)+'F'+str(j)+'B'+str(k)+'X')
                 csv_header.append(str(i)+'F'+str(j)+'B'+str(k)+'Y')
                 csv_header.append(str(i)+'F'+str(j)+'B'+str(k)+'Z')
@@ -50,41 +43,55 @@ def csv_init():
         writer.writerow(csv_header)
 
 
+def callback_node_state(state):
+
+    global node_state
+    node_state = state.data
+
+
 def callback_joint(joint):
-    joint_list = joint.position
-    write_csv()
 
+    global joint_list
+    global node_state
 
-def callback_tactile_0(tactile):
-    tactile_list_0 = tactile.data
-    write_csv()
+    if node_state == 1:
+        # Write to new csv
+        joint_list = joint.position
+        write_csv()
 
 
 def callback_tactile_1(tactile):
     tactile_list_1 = tactile.data
-    write_csv()
 
 
 def callback_tactile_2(tactile):
     tactile_list_2 = tactile.data
-    write_csv()
 
 
 def callback_tactile_3(tactile):
     tactile_list_3 = tactile.data
-    write_csv()
+
+
+def callback_tactile_4(tactile):
+    tactile_list_4 = tactile.data
 
 
 def write_csv():
 
-    if None in joint_list or None in tactile_list_0
-    or None in tactile_list_1 or None in tactile_list_2 or None in tactile_list_3:
+    if None in tactile_list_1
+       #None in tactile_list_2 or
+       #None in tactile_list_3 or
+       #None in tactile_list_4 or
+       or None in joint_list:
         pass
 
     else:
         print ('Fully updated.. publishing\n')
-        JT_List = list(tactile_list_0) + list(tactile_list_1) + list(tactile_list_2) + 
-        list(tactile_list_3) + list(joint_list)
+        JT_List = list(tactile_list_1) +
+                  #list(tactile_list_2) +
+                  #list(tactile_list_3) +
+                  #list(tactile_list_4) +
+                  list(joint_list)
 
         # Write to CSV at once
         with open(csv_name, 'a') as csvfile:
@@ -92,20 +99,21 @@ def write_csv():
             writer.writerow(JT_List)
 
         # Reinitialising Buffer
-        tactile_list_0 = [None]*48*patch_number
         tactile_list_1 = [None]*48*patch_number
-        tactile_list_2 = [None]*48*patch_number
-        tactile_list_3 = [None]*48*patch_number
-        joint_list = [None]*16*patch_number 
+        #tactile_list_2 = [None]*48*patch_number
+        #tactile_list_3 = [None]*48*patch_number
+        #tactile_list_4 = [None]*48*patch_number
+        joint_list = [None]*16*patch_number
 
 
 def listener():
     rospy.init_node('joint_tactile_subscriber', anonymous=True)
+    rospy.Subscriber('allegroHand/node_states', Int32, callback_node_state)
     rospy.Subscriber('allegroHand_0/joint_states', JointState, callback_joint)
-    rospy.Subscriber('tactile_reading_finger_0', Int32MultiArray, callback_tactile_0)
     rospy.Subscriber('tactile_reading_finger_1', Int32MultiArray, callback_tactile_1)
-    rospy.Subscriber('tactile_reading_finger_2', Int32MultiArray, callback_tactile_2)
-    rospy.Subscriber('tactile_reading_finger_3', Int32MultiArray, callback_tactile_3)
+    #rospy.Subscriber('tactile_reading_finger_2', Int32MultiArray, callback_tactile_2)
+    #rospy.Subscriber('tactile_reading_finger_3', Int32MultiArray, callback_tactile_3)
+    #rospy.Subscriber('tactile_reading_finger_4', Int32MultiArray, callback_tactile_4)
     rospy.spin()
 
 
